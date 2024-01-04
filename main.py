a2fd7ef7a398ba105ed3e3d2b8a8421eb102d722
Leo Moreira Silva <leo.moreira@me.com>
2024-01-04 01:21:52 -0300

    uploading results

--- main.py
+++ main.py
@@ -34,18 +34,21 @@ def run(is_test, is_balance, which_models):
         d_train_name = d['train']
         d_val_name = d['val']
         for inst in param_dict['smote_instances']:
+            result_map = init.get_result_map()
             for sca in param_dict['scales']:
                 filename_train = d_train_name + '_' + str(inst) + '_instances_' + sca + '.csv'
                 filename_val = d_val_name + '_' + str(inst) + '_instances_' + sca + '.csv'
+
+                print(filename_val)
+
                 # read files and create corpus
                 data_train = rd.create_corpus('/Users/leosilva/Documents/Estudo/Doutorado/Coimbra/2019-2020/Disciplinas/Thesis/Papers/Logique_Sistemas_Research/dataset/' + filename_train)
                 data_val = rd.create_corpus('/Users/leosilva/Documents/Estudo/Doutorado/Coimbra/2019-2020/Disciplinas/Thesis/Papers/Logique_Sistemas_Research/dataset/' + filename_val)
 
                 features = param_dict['features']
 
-                count = 0
+                # count = 0
                 for feat in features:
-                    result_map = init.get_result_map()
                     X = pd.DataFrame(data_train[feat])
                     y = pd.DataFrame(data_train['daysFromDevOrFixToTest_categories'])
                     # y = pd.DataFrame(data_train['storyPoints'])
@@ -78,88 +81,98 @@ def run(is_test, is_balance, which_models):
 
                             cv = StratifiedKFold(n_splits=f, shuffle=True, random_state=random_state)
 
+                            print("Train test split...")
+                            X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                                                test_size=0.2,
+                                                                                random_state=random_state)
+
                             gc.collect()
 
-                            for train_index, test_index in cv.split(X, y):
-                                X_train_cv = X.iloc[train_index]
-                                X_test_cv = X.iloc[test_index]
-                                y_train_cv = y.iloc[train_index]
-                                y_test_cv = y.iloc[test_index]
+                            # print("x has nan: ", X.isnull().values.any())
+                            # print("y has nan: ", y.isnull().values.any())
+                            # print(y.value_counts())
 
-                                start = time.time()
-                                print("Running model {}".format(model_name))
-                                print(X_train_cv.shape)
-                                print(y_train_cv.shape)
+                            # for train_index, test_index in cv.split(X, y):
+                            #     X_train_cv = X.iloc[train_index]
+                            #     X_test_cv = X.iloc[test_index]
+                            #     y_train_cv = y.iloc[train_index]
+                            #     y_test_cv = y.iloc[test_index]
 
-                                gc.collect()
+                            start = time.time()
+                            # print("Running model {}".format(model_name))
+                            # print(X_train.shape)
+                            # print(y_train.shape)
 
-                                grid_search = ut.perform_grid_search(model, hyperparams, cv, X_train_cv, y_train_cv)
-                                end = time.time()
-                                best_model = grid_search.best_estimator_
-                                print("Training time: {}".format(end - start))
+                            gc.collect()
 
-                                gc.collect()
+                            # grid_search = ut.perform_grid_search(model, hyperparams, cv, X_train, y_train)
+                            model = model.fit(X_train, y_train.astype('int').values.ravel())
+                            end = time.time()
+                            # best_model = grid_search.best_estimator_
+                            print("Training time: {}".format(end - start))
 
-                                start = time.time()
-                                metrics = cross_validate(estimator=model,
-                                                         X=X_train_cv,
-                                                         y=y_train_cv.astype('int').values.ravel(),
-                                                         cv=cv,
-                                                         scoring=init.get_default_scoring(),
-                                                         error_score="raise")
+                            gc.collect()
 
-                                end = time.time()
-                                print("Cross validate time: {}".format(end - start))
+                            start = time.time()
+                            metrics = cross_validate(estimator=model,
+                                                     X=X_train,
+                                                     y=y_train.astype('int').values.ravel(),
+                                                     cv=cv,
+                                                     scoring=init.get_default_scoring(),
+                                                     error_score="raise")
 
-                                test_predictions = best_model.predict(X_test_cv)
-                                class_report_dict_test = classification_report(y_test_cv.astype('int'), test_predictions, output_dict=True)
+                            end = time.time()
+                            print("Cross validate time: {}".format(end - start))
 
-                                val_predictions = best_model.predict(X_val)
-                                class_report_dict_val = classification_report(y_val.astype('int'), val_predictions,
-                                                                          output_dict=True)
+                            test_predictions = model.predict(X_test)
+                            class_report_dict_test = classification_report(y_test.astype('int'), test_predictions, output_dict=True)
 
-                                result_map["Algorithm"].append(model_name)
-                                result_map["Train Accuracy"].append(round(np.mean(metrics['test_accuracy']), 4))
-                                result_map["Train Precision"].append(round(np.mean(metrics['test_precision']), 4))
-                                result_map["Train Recall"].append(round(np.mean(metrics['test_recall']), 4))
-                                result_map["Train F1 Score"].append(round(np.mean(metrics['test_f1_score']), 4))
+                            val_predictions = model.predict(X_val)
+                            class_report_dict_val = classification_report(y_val.astype('int'), val_predictions,
+                                                                      output_dict=True)
 
-                                result_map["Test Accuracy"].append(round(np.mean(class_report_dict_test['accuracy']), 4))
-                                result_map["Test Precision"].append(round(np.mean(class_report_dict_test['weighted avg']['precision']), 4))
-                                result_map["Test Recall"].append(round(np.mean(class_report_dict_test['weighted avg']['recall']), 4))
-                                result_map["Test F1 Score"].append(round(np.mean(class_report_dict_test['weighted avg']['f1-score']), 4))
+                            result_map["Algorithm"].append(model_name)
+                            result_map["Train Accuracy"].append(round(np.mean(metrics['test_accuracy']), 4))
+                            result_map["Train Precision"].append(round(np.mean(metrics['test_precision']), 4))
+                            result_map["Train Recall"].append(round(np.mean(metrics['test_recall']), 4))
+                            result_map["Train F1 Score"].append(round(np.mean(metrics['test_f1_score']), 4))
 
-                                result_map["Val. Accuracy"].append(round(np.mean(class_report_dict_val['accuracy']), 4))
-                                result_map["Val. Precision"].append(
-                                    round(np.mean(class_report_dict_val['weighted avg']['precision']), 4))
-                                result_map["Val. Recall"].append(
-                                    round(np.mean(class_report_dict_val['weighted avg']['recall']), 4))
-                                result_map["Val. F1 Score"].append(
-                                    round(np.mean(class_report_dict_val['weighted avg']['f1-score']), 4))
+                            result_map["Test Accuracy"].append(round(np.mean(class_report_dict_test['accuracy']), 4))
+                            result_map["Test Precision"].append(round(np.mean(class_report_dict_test['weighted avg']['precision']), 4))
+                            result_map["Test Recall"].append(round(np.mean(class_report_dict_test['weighted avg']['recall']), 4))
+                            result_map["Test F1 Score"].append(round(np.mean(class_report_dict_test['weighted avg']['f1-score']), 4))
 
-                                # result_map["AUC"].append(round(np.mean(metrics['test_roc_auc']), 4))
+                            result_map["Val. Accuracy"].append(round(np.mean(class_report_dict_val['accuracy']), 4))
+                            result_map["Val. Precision"].append(
+                                round(np.mean(class_report_dict_val['weighted avg']['precision']), 4))
+                            result_map["Val. Recall"].append(
+                                round(np.mean(class_report_dict_val['weighted avg']['recall']), 4))
+                            result_map["Val. F1 Score"].append(
+                                round(np.mean(class_report_dict_val['weighted avg']['f1-score']), 4))
 
-                                result_map["Bal. Strategy"].append(balance)
-                                result_map["SMOTE Instances"].append(inst)
-                                result_map["Scale"].append(sca)
-                                result_map["Features"].append(feat)
-                                result_map["Folds"].append(f)
-                                result_map["Hyper Params."].append(grid_search.best_params_)
-                                result_map["Model"].append(best_model)
-                                pd.set_option('display.max_colwidth', None)
-                                pd.set_option('display.max_columns', None)
-                                temp_df = pd.DataFrame(result_map)
-                                print(temp_df.tail(1).T)
+                            result_map["AUC"].append(round(np.mean(metrics['test_roc_auc']), 4))
 
-                                gc.collect()
+                            result_map["Bal. Strategy"].append(balance)
+                            result_map["SMOTE Instances"].append(inst)
+                            result_map["Scale"].append(sca)
+                            result_map["Features"].append(feat)
+                            result_map["Folds"].append(f)
+                            # result_map["Hyper Params."].append(grid_search.best_params_)
+                            result_map["Model"].append(model)
+                            pd.set_option('display.max_colwidth', None)
+                            pd.set_option('display.max_columns', None)
+                            temp_df = pd.DataFrame(result_map)
+                            print(temp_df.tail(1).T)
 
-                    result_df = pd.DataFrame(result_map)
-                    result_df.sort_values(by='Val. F1 Score', ascending=False, inplace=True)
+                            gc.collect()
 
-                    ut.save_df_to_csv(result_df, d, inst, sca, count)
-                    ut.save_best_model(result_df, d, inst, sca, count)
+            result_df = pd.DataFrame(result_map)
+            result_df.sort_values(by='Val. F1 Score', ascending=False, inplace=True)
 
-                    count = count + 1
+            ut.save_df_to_csv(result_df, d, inst)
+                    # ut.save_best_model(result_df, d, inst, sca, count)
+
+                    # count = count + 1
 
     utc_dtf = datetime.now(timezone.utc)
     print("GENERAL ANALYSIS ending at {}".format(utc_dtf.astimezone().isoformat()))
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 from sklearn.model_selection import GridSearchCV
 # import joblib
 import pickle
+import os
+
+
+execution = "without_grid_search_c"
 
 
 def perform_grid_search(model, params, cv, X_train_selected, y_train_cv):
@@ -13,10 +17,14 @@ def get_random_state():
     return 42
 
 
-def save_df_to_csv(df, dataset, inst, sca, count):
-    filename = 'best_models/2_5_5/best_models_' + dataset['train'] + '_' + str(inst) + '_instances_' + sca + '_' + str(count) + '.csv'
+def save_df_to_csv(df, dataset, inst):
 
-    df.to_csv(filename, index=None, sep=';', mode='w')
+    if not os.path.exists('best_models/' + execution):
+        os.makedirs('best_models/' + execution)
+
+    filename = 'best_models/' + execution + '/best_models_' + dataset['train'] + '_' + str(inst) + '_instances.csv'
+
+    df.to_csv(filename, index=None, sep=',', mode='w')
 
 
 def save_best_model(result_df, dataset, inst, sca, count):
@@ -27,7 +35,7 @@ def save_best_model(result_df, dataset, inst, sca, count):
     for index, row in models.iterrows():
         m = row['Model']
 
-        filename = 'best_models/2_5_5/model_' + str(index + 1) + '_' + dataset['train'] + '_' + str(inst) + '_instances_' + sca + '_' + str(count) + '.pkl'
+        filename = 'best_models/1_5_5/model_' + str(index + 1) + '_' + dataset['train'] + '_' + str(inst) + '_instances_' + sca + '_' + str(count) + '.pkl'
 
         with open(filename, 'wb') as file:
             pickle.dump(m, file)
